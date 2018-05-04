6d07d481199b8d827aab0cce4330e15df5ddf214
Olatunbosun Adeoye <adex001@gmail.com>
2018-05-04 03:39:03 +0100

    refactored

--- server/model/MealsModel.js
+++ server/model/MealsModel.js
@@ -4,7 +4,7 @@ const mealsArray = [
     mealTitle: 'Rice and Chicken',
     mealDescription: 'Soft parboiled rice and super grilled Chicken',
     mealPrice: 4000,
-    mealImage: 'https://www.tablespoon.com/recipes/wild-rice-and-turkey-casserole/b86a2adc-fd0a-4cc6-9871-b44d081c5e00'
+    mealImage: 'https://www.tablespoon.com/recipes/wild-rice-and-turkey-casserole/b86a2adc-fd0a-4cc6-9871-b44d081c5e00',
 
   },
   {
@@ -12,9 +12,9 @@ const mealsArray = [
     mealTitle: 'Spaghetti with grilled Fish',
     mealDescription: 'fine spaghetti and seasoned grilled Chicken',
     mealPrice: 3000,
-    mealImage: 'https://www.manusmenu.com/spaghetti-with-fish-sauce'
+    mealImage: 'https://www.manusmenu.com/spaghetti-with-fish-sauce',
 
-  }
+  },
 ];
 
 export default mealsArray;
